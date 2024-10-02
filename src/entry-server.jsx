abf3f73ef4f2b23c1b64f20ba6572354623db61f
Meymoun Boualaoui <77726988+MeymounB@users.noreply.github.com>
2024-10-02 06:13:03 +0200

    fix: second try vercel deploy

--- src/entry-server.jsx
+++ src/entry-server.jsx
@@ -1,13 +1,27 @@
 import React from "react";
 import ReactDOMServer from "react-dom/server";
-import App from "./App";
-import "./index.css";
+import App from "@/App";
 
 export function render() {
-  const html = ReactDOMServer.renderToString(
+  const appHtml = ReactDOMServer.renderToString(
     <React.StrictMode>
       <App />
     </React.StrictMode>,
   );
-  return { html };
+
+  return `
+    <!doctype html>
+    <html lang="fr">
+      <head>
+        <meta charset="UTF-8" />
+        <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Vite + React + TS</title>
+      </head>
+      <body>
+        <div id="root">${appHtml}</div>
+        <script type="module" src="/src/entry-client.jsx"></script>
+      </body>
+    </html>
+  `;
 }
