9a3a0286de70ab83d73e8df40620002ead27a61b
Meymoun Boualaoui <77726988+MeymounB@users.noreply.github.com>
2024-10-02 06:02:25 +0200

    fix: imports according to alias @

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import reactLogo from "@/assets/icons/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
+
+import "@/styles/App.css";
+import "@/styles/font.css";
+import "@/styles/index.css";
 
 function App() {
   const [count, setCount] = useState(0);
